22a779495c65dd7c03e581cdb52e97c786793d5e
T0mqNet <tomq.net@gmail.com>
2021-09-02 17:15:12 +0000

    Preview v2

--- script.js
+++ script.js
@@ -1,6 +1,36 @@
 function compute()
+{    
+    // we gather the values from the inputs
+    var principal = document.getElementById("principal").value;
+
+    if (principal <=0){
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+        // we do not do anyhing more, we are waiting
+        // for valid value
+        return;
+    }
+
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+
+    // we calculate interest and year
+    var interest = principal * years * rate /100;
+    var year = new Date().getFullYear()+parseInt(years);
+
+    // we set content of result with text and our calculated values
+    document.getElementById("result").innerHTML=
+        "If you deposit " + principal + ",<br/>"
+        + "at an interest rate of " + rate + "%."
+        + "You will receive an amount of " + interest + ","
+        + "in the year " + year;
+}
+
+function updateRate() 
 {
-    p = document.getElementById("principal").value;
-    
+    // when range input changes we update the numeric value
+    // in the span
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
 }
         
